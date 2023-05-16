f412f2345b32c09a69636ddfa194d1e37b2a7ed0
Bo Bayles <bbayles@gmail.com>
2023-05-16 10:30:26 -0500

    Try once for a Windows build

--- what_url/ada_build.py
+++ what_url/ada_build.py
@@ -1,14 +1,17 @@
 from cffi import FFI
 from os.path import dirname, join
+from sys import platform
 
 file_dir = dirname(__file__)
+
+libraries = ['stdc++'] if platform == 'linux' else []
 
 ffi_builder = FFI()
 ffi_builder.set_source(
     'what_url._ada_wrapper',
     '# include "ada_c.h"',
     include_dirs=[file_dir],
-    libraries=['stdc++'],
+    libraries=libraries,
     extra_objects=[join(file_dir, 'ada.o')],
 )
 
